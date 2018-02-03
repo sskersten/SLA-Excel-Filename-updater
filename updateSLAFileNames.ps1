f3bb3bc23f572e1f55e1a8e0bc07fe67f579229c
Suzy Kersten <suzykersten72@outlook.com>
2018-02-03 11:50:14 -0600

    Added Excel Lesson Plan Support
    Basically now you can use this to update the Excel Workshop dates and it zeroes aout the Activity, Time Required, etc etc.

--- updateSLAFileNames.ps1
+++ updateSLAFileNames.ps1
@@ -1,7 +1,7 @@
 #just the date portion of the filename
 $SINGLEDATE_REGEXP = "\d{1,2}-\d{1,2}"
 #The entire filename used in SLA Excel files
-$SLA_FILENAME_REGEXP = ("[\w\s]*" + $SINGLEDATE_REGEXP + " to " + $SINGLEDATE_REGEXP)
+$SLA_FILENAME_REGEXP = ( $SINGLEDATE_REGEXP + " to " + $SINGLEDATE_REGEXP)
 #old files folder name
 $OLDFILES_FOLDER_NAME = ".\Old_SLA_Files"
 
@@ -20,11 +20,10 @@ foreach ($filename in $filenames) {
 			((get-date -Date $originalFilenameDates[1].value).AddDays(7)).ToString('M-d')
 		
 		#setup new filenames by replacing the old dates with new dates
-		$newFilename = $filename -replace $SINGLEDATE_REGEXP, ($newFilenameDates[0] + " to " + $newFilenameDates[1])
+		$newFilename = $filename -replace $SLA_FILENAME_REGEXP, ($newFilenameDates[0] + " to " + $newFilenameDates[1])
 		
 		#create a new version of the file with the new date in the filename
 		copy-item -Path (".\" + $filename + ".xlsx") -Destination (".\" + $newFilename + ".xlsx")
-            
         write-output ("Updated " + $filename + ".")		
 
         #shove old files into a folder named after the week it's from
@@ -43,27 +42,42 @@ foreach ($filename in $filenames) {
 		move-item (".\"+$filename+".xlsx") ($oldFilesDumpFold + "\" +$filename + ".xlsx") 
 
 
-        #use e
+        #Update Lesson Plan Files
         if ($newFilename.Contains("Lesson Plan")){
-            #Modify the Excel sheets with the new dates as well
+            #Setup Excel sheet objects
             $Excel = New-Object -ComObject Excel.Application
             $ExcelWorkBook = $Excel.workbooks.Open((Convert-Path .) + "\" + $newFilename)
             $ExcelWorkSheet = $Excel.WorkSheets.Item(1)
-            #$ExcelWorkSheet.activate()
-
-            ##modify values of a test cell
+            
             ##Change week date and current date
-            $ExcelWorkSheet.Cells.Item(4,7) = $newFilenameDates[0] + " to " + $newFilenameDates[1]
+            $ExcelWorkSheet.Cells.Item(4,7)  = $newFilenameDates[0] + " to " + $newFilenameDates[1]
             $ExcelWorkSheet.Cells.Item(4,13) = ((get-date -Date $originalFilenameDates[1].value).AddDays(-1)).ToString('M-d')
-            ##change workshop date cells
-            $ExcelWorkSheet.Cells.Item(8,9) = "Workshop Date: " + ((get-date -Date $originalFilenameDates[0].value).AddDays(7)).ToString('M-d')
-            $ExcelWorkSheet.Cells.Item(22,9) ="Workshop Date: " + ((get-date -Date $originalFilenameDates[1].value).AddDays(5)).ToString('M-d')
 
+            ##update individual workshop dates by using values in the cells already
+            $excel_date = $ExcelWorkSheet.Cells.Item(8,9).Text.Substring(15)
+            $excel_date = ((get-date -Date $excel_date).AddDays(7)).toString("MM-dd-yyyy")
+            $ExcelWorkSheet.Cells.Item(8,9) = ("Workshop Date: " + $excel_date)
+
+            $excel_date =  $ExcelWorkSheet.Cells.Item(22,9).Text.Substring(15)
+            $excel_date = ((get-date -Date $excel_date).AddDays(7)).toString("MM-dd-yyyy")
+            $ExcelWorkSheet.Cells.Item(22,9) = ("Workshop Date: " + $excel_date)
+
+            #update Activity Cells
+            $ExcelWorksheet.Cells.Item(9,9)  = "Activity:";
+            $ExcelWorksheet.Cells.Item(9,13) = "Time Required:"
+            $ExcelWorksheet.Cells.Item(14,9) = "Materials Required:"
+            $ExcelWorksheet.Cells.Item(17,9) = "How will this activty be linked to the course material:"
+            $ExcelWorksheet.Cells.Item(23,9) = "Activity:";
+            $ExcelWorksheet.Cells.Item(23,13)= "Time Required:"
+            $ExcelWorksheet.Cells.Item(28,9) = "Materials Required:"
+            $ExcelWorksheet.Cells.Item(31,9) = "How will this activty be linked to the course material:"
+
+            #quit Excel
             $ExcelWorkBook.Save()
             $ExcelWorkBook.Close()
             $Excel.Quit()
             [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
             Stop-Process -Name EXCEL -Force
-        }
-	} #end of If
+        } #end of if Lesson Plan 
+	} #end of If SLA file
 } #end of foreach
